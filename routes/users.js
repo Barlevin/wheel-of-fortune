d5143b9f9a7a57aa92c0096dcf86d8996a4068fc
Bar <barlevinn@gmail.com>
2025-06-10 10:26:43 +0300

    new version

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('daniel')
   res.send('respond with a resource');
+
 });
 
 module.exports = router;
